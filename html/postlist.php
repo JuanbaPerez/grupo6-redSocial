4ff619b9b691251a67e3da19c8b9921e0d60378d
marcoskipper <50128216+marcoskipper@users.noreply.github.com>
2019-06-23 20:11:55 -0300

    cambios

---
+++ html/postlist.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>POST | LOGFOTO</title>
+</head>
+  <body>
+    <header>
+      <?php
+
+        include_once ("php/sidebar.php")
+
+       ?>
+    </header>
+<main>
+  <article>
+      <div class="post-header">
+          <div class="profilepicture">
+
+            <img src="../img/profilepic.jpg" alt="">
+
+          </div>
+
+          <p class="username">User name</p>
+          <p class="ago">20 minutes</p>
+      </div>
+        <div class="postimg">
+          <img src="../img/nochefotos.jpg" alt="postimg">
+
+        </div>
+
+
+<div class="posticons">
+<i class="fa fa-heart"></i>
+<i class="fa fa-comment"></i>
+
+
+</div>
+
+
+<div class="post-footer">
+          </div>
+          <div class="comments">
+              <ul>
+                  <li>Wow</li>
+                  <li>Amazing</li>
+                  <li>Love this!</li>
+              </ul>
+          </div>
+
+  </article>
+</main>
+
+
+  </body>
+</html>
