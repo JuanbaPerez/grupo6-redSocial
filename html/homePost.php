f577cd0e74d401331d0aa9440d9f9b6dafda337f
Juanba Perez <juanbaperez@MacBook-Pro-de-Juanba-3.local>
2019-06-30 18:03:53 -0300

    Haciendo la vista de posteos, organizando el css. los MQ de tablet esta rota la imagen. preguntar lando

---
+++ html/homePost.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>POST | MyFuture</title>
+</head>
+
+
+  <body>
+    <header>
+      <?php
+
+        include_once ("php/sidebar.php")
+
+       ?>
+    </header>
+
+<main>
+
+  <div class="postContainer">
+
+
+    <div class="photoProfileContainer">
+      <img class="profilePicture" src="../img/profilepic.jpg" alt="profile">
+
+      <div class="userName">
+        <h2>Marcelo Diaz</h2>
+        <p>1hs</p>
+      </div>
+
+    </div>
+
+    <div class="postImage">
+        <img src="../img/nochefotos.jpg" alt="postFoto">
+    </div>
+
+
+<div class="postIcons">
+
+  <div class="postIcons-heart">
+    <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 38</i></button>
+
+  </div>
+
+
+  <div class="postIcons-comment">
+
+    <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 5</i></button>
+
+  </div>
+</div>
+
+
+  <div class="comments">
+    <hr>
+    <p><strong>Margater.jackson:</strong> NICE</p>
+    <hr>
+    <p><strong>JackieSharp:</strong> where is that?</p>
+    <hr>
+    <p><strong>Chuck-Lid:</strong> Miss you bud!</p>
+  </div>
+
+
+  </div>
+
+
+  <div class="postContainer">
+
+
+    <div class="photoProfileContainer">
+      <img class="profilePicture" src="../img/franky.jpg" alt="profile">
+
+      <div class="userName">
+        <h2>Franky Funk</h2>
+        <p>30 minutes</p>
+      </div>
+
+    </div>
+
+    <div class="postImage">
+        <img src="../img/pantano.jpg" alt="postFoto">
+    </div>
+
+
+<div class="postIcons">
+
+  <div class="postIcons-heart">
+    <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 30</i></button>
+
+  </div>
+
+
+  <div class="postIcons-comment">
+
+    <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 12</i></button>
+
+  </div>
+</div>
+
+
+  <div class="comments">
+    <hr>
+    <p><strong>Glendon-Get:</strong> Awesome</p>
+    <hr>
+    <p><strong>LoneWold:</strong> is there a ghost there?</p>
+    <hr>
+    <p><strong>Gaming.gx:</strong> Creepy</p>
+  </div>
+
+
+  </div>
+
+  <div class="postContainer">
+
+
+    <div class="photoProfileContainer">
+      <img class="profilePicture" src="../img/profilepic.jpg" alt="profile">
+
+      <div class="userName">
+        <h2>Marcelo Diaz</h2>
+        <p>1hs</p>
+      </div>
+
+    </div>
+
+    <div class="postImage">
+        <img src="../img/nochefotos.jpg" alt="postFoto">
+    </div>
+
+
+<div class="postIcons">
+
+  <div class="postIcons-heart">
+    <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 38</i></button>
+
+  </div>
+
+
+  <div class="postIcons-comment">
+
+    <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 5</i></button>
+
+  </div>
+</div>
+
+
+  <div class="comments">
+    <hr>
+    <p><strong>Margater.jackson:</strong> NICE</p>
+    <hr>
+    <p><strong>JackieSharp:</strong> where is that?</p>
+    <hr>
+    <p><strong>Chuck-Lid:</strong> Miss you bud!</p>
+  </div>
+
+
+  </div>
+
+
+  <div class="postContainer">
+
+
+    <div class="photoProfileContainer">
+      <img class="profilePicture" src="../img/franky.jpg" alt="profile">
+
+      <div class="userName">
+        <h2>Franky Funk</h2>
+        <p>30 minutes</p>
+      </div>
+
+    </div>
+
+    <div class="postImage">
+        <img src="../img/pantano.jpg" alt="postFoto">
+    </div>
+
+
+<div class="postIcons">
+
+  <div class="postIcons-heart">
+    <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 30</i></button>
+
+  </div>
+
+
+  <div class="postIcons-comment">
+
+    <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 12</i></button>
+
+  </div>
+</div>
+
+
+  <div class="comments">
+    <hr>
+    <p><strong>Glendon-Get:</strong> Awesome</p>
+    <hr>
+    <p><strong>LoneWold:</strong> is there a ghost there?</p>
+    <hr>
+    <p><strong>Gaming.gx:</strong> Creepy</p>
+  </div>
+
+
+  </div>
+
+
+
+
+</main>
+
+
+
+<?php
+
+  include_once ("php/footer.php")
+
+ ?>
+
+
+  </body>
+</html>
---
+++ html/php/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer-distributed">
+
+  <div class="footer-left">
+
+    <p class="footer-links">
+      <a href="home.html">Home</a>
+      ·
+      <a href="contact.html">Contact us</a>
+      ·
+      <a href="login.html">Log In</a>
+      ·
+      <a href="registration.html">Registrer</a>
+      ·
+      <a href="FAQ.html">Faq</a>
+    </p>
+
+    <p class="footer-company-name">LogFoto &copy; 2019</p>
+
+  </div>
+
+  <div class="footer-right">
+
+    <form method="get" action="#">
+      <input placeholder="Search our website" name="search" />
+      <i class="fa fa-search"></i>
+    </form>
+
+  </div>
+
+</footer>
