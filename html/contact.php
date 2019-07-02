0a199274e6b85f7be8163fb0b6caa50f87b5220b
Juanba Perez <juanbaperez@MacBook-Pro-de-Juanba-3.local>
2019-07-02 20:52:37 -0300

    haciendo un fix de la foto del posteo ya que estaba bugueada

--- html/contact.php
+++ html/contact.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css?v=02-07-2019">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>CONTACT | MyFuture</title>
   </head>
