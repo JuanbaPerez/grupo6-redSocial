26f0cd06a71e4abdd2cdf3de3f950dbd7f1d7423
Juanba Perez <juanbaperez@MacBook-Pro-de-Juanba-3.local>
2019-07-21 15:48:59 -0300

    se arranca el postDetail hay que ajustarlo

--- html/postDetails.php
+++ html/postDetails.php
@@ -1,17 +1,89 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <main>
-      <div class="postImageDetails">
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Document</title>
+</head>
+<body>
+
+  <header>
+
+  </header>
+
+
+  <main>
+
+    <div class="postContainer">
+
+
+      <div class="photoProfileContainer">
+        <img class="profilePicture" src="../img/profilepic.jpg" alt="profile">
+
+        <div class="userName">
+          <h2>Marcelo Diaz</h2>
+          <p>1hs</p>
+        </div>
+
+      </div>
+
+      <div class="postImage">
           <img src="../img/nochefotos.jpg" alt="postFoto">
       </div>
 
 
+  <div class="postIcons">
+
+    <div class="postIcons-heart">
+      <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 38</i></button>
+
+    </div>
+
+
+    <div class="postIcons-comment">
+
+      <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 5</i></button>
+
+    </div>
+
+
+
+  </div>
+
+
+  <div class="commentContainer">
+
+      <div class="comments">
+        <!-- <img src="../img/franky.jpg" alt=""> -->
+        <p>Juanba: asd</p>
+
+      </div>
+
+      <div class="comments">
+        <p>Juanba: asd</p>
+
+      </div>
+
+      <div class="comments lastComment">
+
+        <p>Juanba: asd</p>
+      </div>
+  </div>
+
+
+
+    </div>
+
     </main>
 
-  </body>
+
+
+    <footer>
+
+      //include_once.
+    </footer>
+</body>
 </html>
