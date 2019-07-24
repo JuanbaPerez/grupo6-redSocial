0f3b977253da215a964df7d72cd88368816b4407
mateo <mateghidini123@gmail.com>
2019-07-24 18:03:22 -0300

    Home terminado con texto, titulo e imagen a la derecha

--- html/index.php
+++ html/index.php
@@ -18,23 +18,25 @@
 
      ?>
   </header>
+<div class="main-container">
 
-<div class="photoContainer">
 
-  <img src="../img/logo.png" alt="logo logFoto">
+  <div class="photoContainer2">
+
+    <img src="../img/logo.png" alt="logo logFoto">
+  </div>
+
+  <main>
+    <div class="titleHome">
+      <h1>MyFuture</h1>
+
+    </div>
+
+    <div class="textHome">
+
+      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+    </div>
 </div>
-
-<main>
-  <div class="titleHome">
-    <h1>MyFuture</h1>
-
-  </div>
-
-  <div class="textHome">
-
-    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-  </div>
-
 
 
 
